93d63b44f44571b7553c9fd9a1caa749ed5e71e0
Anxhul10 <anxhul10@gmail.com>
2026-01-18 18:07:00 +0530

    fix: filters of pkg paths

--- src/find_pkg_json.rs
+++ src/find_pkg_json.rs
@@ -4,15 +4,23 @@ use std::path::PathBuf;
 pub fn find_pkg_json(filters: Vec<&str>) -> Vec<PathBuf> {
     let mut pkg_paths: Vec<PathBuf> = Vec::new();
     for entry in glob("**/package.json").expect("failed to read glob patterns") {
+        let mut flag = true;
         for to_filter in &filters {
             match entry {
                 Ok(ref path) => {
-                    if path.display().to_string().contains(to_filter) == false {
-                        pkg_paths.push(path.to_path_buf());
-                        break;
+                    if path.display().to_string().contains(to_filter) {
+                        flag = false;
                     }
                 },
                 Err(ref e) => println!("{:?}", e)
+            }
+        }
+        if flag {
+            match entry {
+                Ok(path) => {
+                    pkg_paths.push(path);
+                },
+                Err( e) => println!("{:?}", e)
             }
         }
     }
