df428832892e4c9214e89bcc8128f238dbae7954
Anxhul10 <anxhul10@gmail.com>
2026-01-20 23:35:44 +0530

    chore: add payload reader

---
+++ src/read_payload.rs
@@ -0,0 +1,18 @@
+use std::{
+    fs::File,
+    io::BufReader,
+    path::Path,
+    error::Error
+};
+use serde_json::Value;
+
+pub fn main<P: AsRef<Path>>(path: P) -> Result<Value, Box<dyn Error>> {
+    // Open file in RO mode with buffer
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+
+    // Read the JSON contents of the file
+    let u = serde_json::from_reader(reader)?;
+
+    Ok(u)
+}
