fa19be5a7afe405b6254da19d90345cafaf9f7c7
Anxhul10 <anxhul10@gmail.com>
2026-01-22 02:09:24 +0530

    chore: add dev, peer, deps fn

--- src/lib.rs
+++ src/lib.rs
@@ -13,28 +13,56 @@ fn get_pkg_name(path: String) -> String{
     name
 }
 
+fn get_tree_deps(path: String) -> String {    
+    let payload: Value = match utils::read_payload::read_payload(path) {
+        Ok(p) => p,
+        Err(_) => return String::new(),
+    };
+
+    match payload.get("dependencies") {
+        Some(v) => v.to_string(),
+        None => String::new(),
+    }
+}
+
+fn get_tree_dev(path: String, current_pkg: String) -> String {
+    let payload: Value = match utils::read_payload::read_payload(path) {
+        Ok(p) => p,
+        Err(_) => return String::new(),
+    };
+
+    match payload.get("devDependencies") {
+        Some(v) => v.to_string(),
+        None => String::new(),
+    }
+}
+
+fn get_tree_peer(path: String) -> String {    
+    let payload: Value = match utils::read_payload::read_payload(path) {
+        Ok(p) => p,
+        Err(_) => return String::new(),
+    };
+
+    match payload.get("peerDependencies") {
+        Some(v) => v.to_string(),
+        None => String::new(),
+    }
+}
 #[neon::export]
 fn runner(name: String) -> String {
     let mut pkg_names = Vec::new();
     let filter = vec![ ".yarn", "node_modules"];
     let paths = find_pkg_json::find_pkg_json(filter);
-    for path in paths {
-        pkg_names.push(get_pkg_name(path));
+    for path in paths.clone() {
+        pkg_names.push(get_pkg_name(path.clone()));
+    }
+
+    let mut pkg_tracker = 0;
+    for path in paths.clone() {
+        let current_pkg = pkg_names[pkg_tracker].clone();
+        let result = get_tree_dev(path.clone(), current_pkg).clone();
+        println!("{}", result);
+        pkg_tracker += 1;
     }
     format!("hello {name}")
 }
-
-// Use #[neon::main] to add additional behavior at module loading time.
-// See more at: https://docs.rs/neon/latest/neon/attr.main.html
-// fn temp() -> Result<()> {
-//     for entry in glob("**/package.json")? {
-//         println!("{}", entry?.display());
-//     }
-//     Ok(())
-// }
-
-// #[neon::main]
-// fn main(mut cx: ModuleContext) -> NeonResult<()> {
-//     println!("module is loaded!");
-//     Ok(())
-// }
